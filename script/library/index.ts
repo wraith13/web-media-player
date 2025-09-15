7ed07a28fcbc02bf86b64910c8934e110d69e3b8
道化師 <wraith13@hotmail.co.jp>
2025-09-16 02:03:41 +0900

    🛠️ 実装 ( WIP )

--- script/library/index.ts
+++ script/library/index.ts
@@ -2,10 +2,12 @@ import * as ImportedLocale from "./locale";
 import * as ImportedUI from "./ui";
 import * as ImportedControl from "./control";
 import * as ImportedSvg from "./svg";
+import * as ImportedShortcuts from "./shortcuts";
 export namespace Library
 {
     export import Locale = ImportedLocale.Locale;
     export import UI = ImportedUI.UI;
     export import Control = ImportedControl.Control;
     export import Svg = ImportedSvg.Svg;
+    export import Shortcuts = ImportedShortcuts.Shortcuts;
 }
---
+++ script/library/shortcuts.ts
@@ -0,0 +1,102 @@
+import shortcuts from "@resource/shortcuts.json";
+export namespace Shortcuts
+{
+    export type Entry = (typeof shortcuts)[number]["shortcuts"][number];
+    export type CommandKey = Entry["command"];
+    export type CommandMap = { [key in Shortcuts.CommandKey]-?: () => void };
+    let currentCommandMap: CommandMap | null = null;
+    const keyDisplayNames =
+    {
+        "ArrowUp": "↑",
+        "ArrowDown": "↓",
+        "ArrowLeft": "←",
+        "ArrowRight": "→",
+        " ": "Space",
+        "Control": "Ctrl",
+    };
+    const getDisplayKeyName = (key: string) => keyDisplayNames[key as keyof typeof keyDisplayNames] ?? key;
+    export const getDisplayList = () =>
+        shortcuts.map
+        (
+            i =>
+            ({
+                keyss: i.shortcuts.map(j => j.keys.map(key => getDisplayKeyName(key))),
+                description: i.description,
+            })
+        );
+    const isInputElementFocused = () =>
+        ["input", "textarea", "button"].includes(document.activeElement?.tagName?.toLowerCase() ?? "");
+    const normalizeKey = (key: string, code: string) =>
+        code === "Space" ? " ":
+        key.length === 1 ? key.toUpperCase():
+        key;
+    let pressedKeys: string[] = [];
+    const getShortcutKeys = (type: "onKeyDown" | "onKeyUp", normalizedKey: string) =>
+    {
+        switch(type)
+        {
+        case "onKeyDown":
+            pressedKeys.push(normalizedKey);
+            return pressedKeys;
+        case "onKeyUp":
+            const result = [...pressedKeys];
+            pressedKeys = pressedKeys.filter(i => i !== normalizedKey);
+            return result;
+        }
+    }
+    export const handleKeyEvent = (type: "onKeyDown" | "onKeyUp", event: KeyboardEvent) =>
+    {
+        const commandMap = currentCommandMap;
+        if (null !== commandMap)
+        {
+            const normalizedKey = normalizeKey(event.key, event.code);
+            const shortcutKeys = getShortcutKeys(type, normalizedKey);
+            if ( ! isInputElementFocused())
+            {
+                const commandKeys = shortcuts.reduce((a, b) => a.concat(b.shortcuts), [] as Entry[]).filter
+                (
+                    shortcut =>
+                        shortcut.keys.length === shortcutKeys.length &&
+                        shortcut.keys.every(key => shortcutKeys.includes(key)) &&
+                        type === shortcut.type
+                )
+                .map(i => i.command);
+                if (0 < commandKeys.length)
+                {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                commandKeys.forEach
+                (
+                    i =>
+                    {
+                        console.log("👆 KeyboardShortcut:", i, type, pressedKeys);
+                        const command = commandMap[i];
+                        if (command)
+                        {
+                            command();
+                        }
+                        else
+                        {
+                            console.error("🦋 FIXME: Shortcuts.handleKeyEvent.NotFoundCommand", i);
+                        }
+                    }
+                );
+                if ("onKeyDown" === type && commandKeys.length <= 0 && ! ["Shift", "Control"].includes(normalizedKey))
+                {
+                    console.log("💡 UnknownKeyDown:", pressedKeys);
+                    commandMap["unknownKeyDown"]?.();
+                }
+            }
+        }
+    }
+    export const initialize = () =>
+    {
+        window.addEventListener("keydown", (event) => Shortcuts.handleKeyEvent("onKeyDown", event));
+        window.addEventListener("keyup", (event) => Shortcuts.handleKeyEvent("onKeyUp", event));
+    };
+    export const setCommandMap = (commandMap: CommandMap | null) =>
+    {
+        currentCommandMap = commandMap;
+    };
+}
