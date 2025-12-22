d8201549158b5c96d1f64d627b8d4dcc6f168823
道化師 <wraith13@hotmail.co.jp>
2025-12-23 04:54:59 +0900

    🛠️ 実装 ( WIP )

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "web-media-player-20251222073928";
+const CACHE_NAME = "web-media-player-20251222215018";
 const REGULAR_ASSETS = // embeded from ./resouce/regular-assets.json
 [
     "./",
